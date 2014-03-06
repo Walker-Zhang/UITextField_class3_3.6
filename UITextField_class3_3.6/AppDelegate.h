bd1b08db5c083aefb7f7b9843f7ad14e5aab7143
xiang_jj <xiang_jj@xiang-jjtekiMacBook-Air.local>
2014-03-06 15:36:42 +0800

    Initial Commit

---
+++ UITextField_class3_3.6/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITextField_class3_3.6
+//
+//  Created by xiang_jj on 14-3-6.
+//  Copyright (c) 2014年 xiang_jj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITextField_class3_3.6Tests/UITextField_class3_3_6Tests.m
@@ -0,0 +1,34 @@
+//
+//  UITextField_class3_3_6Tests.m
+//  UITextField_class3_3.6Tests
+//
+//  Created by xiang_jj on 14-3-6.
+//  Copyright (c) 2014年 xiang_jj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITextField_class3_3_6Tests : XCTestCase
+
+@end
+
+@implementation UITextField_class3_3_6Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
